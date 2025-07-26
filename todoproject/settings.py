99abec79fb9362e1727b72e94f0a41059d668205
Krish Choukse <Krishchoukse1107@gmail.com>
2025-07-26 23:27:01 +0530

    rPlease ente" the commit message for your changes. Lines starting

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(flfm62vr8(#gbc#j7u+q9wmpa8#lig7z(h#jkpz(+ogzy@@)g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
